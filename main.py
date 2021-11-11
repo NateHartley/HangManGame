a7e58af9bb797e8df55b220b8fc779407f90f4c1
NateHartley <MiniMax-03>
2021-11-11 19:22:59 +0000

    If work is too long, game will loop. Visuals for wrong guesses are printed.

--- main.py
+++ main.py
@@ -1,48 +1,59 @@
-word = str(input("Enter a word for the other player to guess: "))
-print("\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n"
-      "\n")
+import hangmanvisuals
 
-wrong_guesses = 0
-guess_list = []
+while True:
+    word = str(input("Enter a word for the other player to guess: "))
 
-while wrong_guesses < 10:
+    if len(word) <= 15:
 
-    guess = str(input("Guess a letter: "))
-    length = len(guess)
+        print("\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n"
+              "\n")
 
-    if length == 1:  # if the length of the character is 1
+        wrong_guesses = 0
+        guess_list = []
 
-        if guess in guess_list:  # if character has already been guessed before
-            print("This letter has already been guessed ")
+        while wrong_guesses < 10:
 
-        else:  # if character has not already been guessed before
-            guess_list = guess_list.append(guess)
+            guess = str(input("Guess a letter: "))
+            length = len(guess)
 
-            if guess in word:  # if the guessed character is in the word
-                print(guess, "does appear in the word!")
-                # word.split()??
-                # print out set of ----- with length of word
-                # if guess is in word, replace - with guess in same index as it occurs in word
+            if length == 1:  # if the length of the character is 1
 
-            else:  # if the guessed character is not in the word
-                print("Sorry, ", guess, " is not in the word.\n")
-                print("You have", (9 - wrong_guesses), "wrong guesses remaining.")
-                wrong_guesses = wrong_guesses + 1
+                if guess in guess_list:  # if character has already been guessed before
+                    print("This letter has already been guessed ")
 
-    else:  # if the length of the character is not equal to 1
-        print("--Please enter a single character--\n")
+                else:  # if character has not already been guessed before
+                    # using append() and assigning it to a variable will always return None for some reason lol
+                    guess_list.append(guess)
+                    print("You're guesses are:", guess_list)
 
-# while loop has stopped, maximum number of wrong guesses achieved
-print("You ran out of guesses, you lose!")
+                    if guess in word:  # if the guessed character is in the word
+                        print(guess, "does appear in the word!")
+                        # word.split()??
+                        # print out set of ----- with length of word
+                        # if guess is in word, replace - with guess in same index as it occurs in word
+
+                    else:  # if the guessed character is not in the word
+                        print("Sorry, ", guess, " is not in the word.\n")
+                        print("You have", (9 - wrong_guesses), "wrong guesses remaining.\n")
+                        print(hangmanvisuals.guess_one(wrong_guesses))
+                        wrong_guesses = wrong_guesses + 1
+
+            else:  # if the length of the character is not equal to 1
+                print("--Please enter a single character--\n")
+
+        # while loop has stopped, maximum number of wrong guesses achieved
+        print("You ran out of guesses, you lose!")
+    else:
+        print("That word is too long, please pick another ")

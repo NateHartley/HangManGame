7db30778413772f548d0c6dd88e3ad9abdc3538d
Annabel Hartley (UG) <c0031586@newcastle.ac.uk>
2022-01-21 01:01:24 +0000

    No more None Type error. Word_list now contains word as a list with no duplicates.

--- main.py
+++ main.py
@@ -1,68 +1,45 @@
-import hangmanvisuals
+word = str(input("Enter a word for the other player to guess: "))
+for i in range(1, 15):
+    print('\n')
 
-while True:
-    word = str(input("Enter a word for the other player to guess: "))
-    word_list = list(word)
-    i = 0
 
-    if len(word) <= 15:
+wrong_guesses = 0
+guess_list = []
+word = list(dict.fromkeys(word))  # removes the duplicate letters from the word
+word_list = []
 
-        print("\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n"
-              "\n")
+while wrong_guesses < 10:
 
-        wrong_guesses = 0
-        guess_list = []
-        print("The number of letters in the word is: ", len(word))
+    guess = str(input("Guess a letter: "))
+    length = len(guess)
 
-        while wrong_guesses < 10:
+    if length == 1:  # if the length of the character is 1
 
-            blanks = []
-            for i in word_list:
-                blanks.append("_")
-            print(blanks)
+        if guess in guess_list:  # if character has already been guessed before
+            print("This letter has already been guessed ")
+            print(guess_list)
 
-            guess = str(input("Guess a letter: "))
-            length = len(guess)
+        else:  # if character has not already been guessed before
+            guess_list.append(guess)  # just fyi, this cant be guess_list = guess_list.append(guess), as it turns the list into a None type
 
-            if length == 1:  # if the length of the character is 1
+            if guess in word:  # if the guessed character is in the word
+                print(guess, "does appear in the word!")
+                for i in word:
+                    word_list.append(i)
+                word_list = list(dict.fromkeys(word_list))
+                print(word_list)
+                print(guess_list)
+                if word_list in guess_list:
+                    print('You have guessed all the correct letters! The word was ',word)
+                    break
 
-                if guess in guess_list:  # if character has already been guessed before
-                    print("This letter has already been guessed ")
+            else:  # if the guessed character is not in the word
+                print("Sorry, ", guess, " is not in the word.\n")
+                print("You have", (9 - wrong_guesses), "wrong guesses remaining.")
+                wrong_guesses = wrong_guesses + 1
 
-                else:  # if character has not already been guessed before
-                    # using append() and assigning it to a variable will always return None for some reason lol
-                    guess_list.append(guess)
-                    print("You're guesses are:", guess_list)
+    else:  # if the length of the character is not equal to 1
+        print("--Please enter a single character--\n")
 
-                    if guess in word_list:  # if the guessed character is in the word
-                        print(guess, "does appear in the word!")
-
-                        # word.split()??
-                        # print out set of ----- with length of word
-                        # if guess is in word, replace - with guess in same index as it occurs in word
-
-                    else:  # if the guessed character is not in the word
-                        print("Sorry, ", guess, " is not in the word.\n")
-                        print("You have", (9 - wrong_guesses), "wrong guesses remaining.\n")
-                        print(hangmanvisuals.guess_one(wrong_guesses))
-                        wrong_guesses = wrong_guesses + 1
-
-            else:  # if the length of the character is not equal to 1
-                print("--Please enter a single character--\n")
-
-        # while loop has stopped, maximum number of wrong guesses achieved
-        print("You ran out of guesses, you lose!")
-    else:
-        print("That word is too long, please pick another ")
+# while loop has stopped, maximum number of wrong guesses achieved
+print("You ran out of guesses, you lose!")
